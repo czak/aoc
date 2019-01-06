5abbe62371bd07097fd2acf4b1fe892aea60c1ad
Łukasz Adamczak <lukasz@czak.pl>
2019-01-06 15:05:39 +0100

    2017 day 6 in cpp

--- 2017/cpp/day06.cpp
+++ 2017/cpp/day06.cpp
@@ -2,23 +2,58 @@
 #include <algorithm>
 #include <iterator>
 #include <array>
-using namespace std;
+#include <unordered_set>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <utility>
 
-constexpr int N = 4;
-using bank_array = array<int, N>;
+using bank_array = std::vector<int>;
 
-void print_banks(const bank_array& banks) {
-  copy(begin(banks), end(banks), ostream_iterator<int>(cout, " "));
-  cout << "\n";
+struct bank_hash {
+  size_t operator()(const bank_array& banks) const {
+    size_t h = 0;
+    for (auto& b: banks) {
+      h ^= std::hash<int>{}(b);
+    }
+    return h;
+  }
+};
+
+std::ostream& operator<<(std::ostream& os, const bank_array& banks) {
+  std::copy(banks.begin(), banks.end(), std::ostream_iterator<int>(os, " "));
+  return os;
 }
 
-int part1(const bank_array& banks) {
+std::pair<int, int> solve(bank_array& banks) {
   int cycles = 0;
-  // TODO: not implemented
-  return cycles;
+  std::unordered_set<bank_array, bank_hash> seen {banks};
+  std::vector<bank_array> vseen {banks};
+  while (true) {
+    auto it = std::max_element(banks.begin(), banks.end());
+    auto max = *it;
+    *it = 0;
+    while (max--) {
+      if (++it == banks.end()) it = banks.begin();
+      (*it)++;
+    }
+    cycles++;
+    std::cout << "After cycle " << cycles << ": " << banks << "\n";
+    if (seen.count(banks)) {
+      auto first = find(vseen.begin(), vseen.end(), banks);
+      return {cycles, std::distance(first, vseen.end())};
+    }
+    seen.insert(banks);
+    vseen.push_back(banks);
+  }
 }
 
 int main() {
-  bank_array banks {0, 2, 7, 0};
-  cout << part1(banks) << "\n";
+  bank_array banks {};
+  int b;
+  while (std::cin >> b) {
+    banks.push_back(b);
+  }
+  auto res = solve(banks);
+  std::cout << res.first << ", " << res.second << "\n";
 }
