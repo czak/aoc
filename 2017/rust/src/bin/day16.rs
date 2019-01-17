870aaeb57805eed953b5b27465d897f4eaba9946
Łukasz Adamczak <lukasz@czak.pl>
2019-01-17 21:33:51 +0100

    2017 day 16 in Rust

--- 2017/rust/src/bin/day16.rs
+++ 2017/rust/src/bin/day16.rs
@@ -1,5 +1,7 @@
 use std::io::{self, Read};
 use std::str::FromStr;
+use std::collections::VecDeque;
+use std::iter::Iterator;
 
 #[derive(Debug)]
 enum Step {
@@ -9,7 +11,7 @@ enum Step {
 }
 
 impl FromStr for Step {
-    type Err = String;
+    type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s.chars().nth(0) {
@@ -23,7 +25,7 @@ impl FromStr for Step {
                 let n2: char = s.as_bytes()[3] as char;
                 Ok(Step::Partner(n1, n2))
             },
-            _ => panic!(),
+            _ => Err(()),
         }
     }
 }
@@ -32,7 +34,38 @@ fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
 
-    for s in input.split(",") {
-        println!("{:?}", Step::from_str(s));
+    // let input = "s1,x3/4,pe/b";
+
+    let mut programs: VecDeque<char> =
+        (('a' as u8)..=('p' as u8)).map(|c| c as char).collect();
+
+    let steps: Vec<Step> = input.split(",").map(|s| s.parse().unwrap()).collect();
+
+    for i in 1.. {
+        for s in steps.iter() {
+            match *s {
+                Step::Spin(count) => {
+                    for _ in 0..count {
+                        let p = programs.pop_back().unwrap();
+                        programs.push_front(p);
+                    }
+                },
+                Step::Exchange(a, b) => {
+                    programs.swap(a, b);
+                },
+                Step::Partner(p, q) => {
+                    let a = programs.iter().position(|&c| c == p).unwrap();
+                    let b = programs.iter().position(|&c| c == q).unwrap();
+                    programs.swap(a, b);
+                },
+            }
+        }
+
+        let s = programs.iter().collect::<String>();
+        if s.starts_with("knmdfoij") {
+            println!("{}: {} ----- ", i, s);
+        } else {
+            println!("{}: {}", i, s);
+        }
     }
 }
