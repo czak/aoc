55d5619e5d792fcbb6308123f09f13b57eacc5ae
Łukasz Adamczak <lukasz@czak.pl>
2019-12-06 12:39:07 +0100

    2019 day 06

--- 2019/rust/src/bin/day06.rs
+++ 2019/rust/src/bin/day06.rs
@@ -1,20 +1,24 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 #[macro_use]
 extern crate maplit;
 
 type OrbitMap<'a> = HashMap<&'a str, Vec<&'a str>>;
+type ParentMap<'a> = HashMap<&'a str, &'a str>;
 
 fn main() {
     let mut map = OrbitMap::new();
+    let mut parent_map = ParentMap::new();
     for (key, value) in include_str!("../../../in/day06.in")
         .lines()
         .map(|l| l.split(')'))
         .map(|mut s| (s.next().unwrap(), s.next().unwrap()))
     {
         map.entry(key).or_insert(vec![]).push(value);
+        parent_map.insert(value, key);
     }
 
     println!("Part 1: {}", count(&map, "COM", 0));
+    println!("Part 2: {}", distance(&parent_map, "YOU", "SAN"));
 }
 
 fn count(map: &OrbitMap, base: &str, current: u32) -> u32 {
@@ -25,6 +29,24 @@ fn count(map: &OrbitMap, base: &str, current: u32) -> u32 {
         }
     }
     total
+}
+
+fn path<'a>(parent_map: &'a ParentMap, mut current: &'a str, end: &str) -> Vec<&'a str> {
+    let mut path = Vec::new();
+    while current != end {
+        current = parent_map.get(current).unwrap();
+        path.push(current);
+    }
+    path
+}
+
+fn distance(parent_map: &ParentMap, a: &str, b: &str) -> u32 {
+    let a_path: HashSet<&str> = path(&parent_map, a, "COM").iter().cloned().collect();
+    let b_path: HashSet<&str> = path(&parent_map, b, "COM").iter().cloned().collect();
+
+    let common_len = a_path.intersection(&b_path).count();
+
+    (a_path.len() + b_path.len() - 2 * common_len) as u32
 }
 
 #[cfg(test)]
