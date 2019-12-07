fb9f77c8ade601c171d96fcf621330dcdb48bfd9
Łukasz Adamczak <lukasz@czak.pl>
2019-12-07 10:21:22 +0100

    DRY the intcode computer

--- 2019/rust/src/intcode.rs
+++ 2019/rust/src/intcode.rs
@@ -8,6 +8,24 @@ pub fn run(mut mem: Vec<i32>, input: Vec<i32>) -> Vec<i32> {
 
     let mut output: Vec<i32> = Vec::new();
 
+    fn parse_opcode(opcode: i32) -> (bool, bool, bool, i32) {
+        (
+            ((opcode / 100) % 10 == 1),
+            ((opcode / 1000) % 10 == 1),
+            ((opcode / 10000) % 10 == 1),
+            opcode % 100,
+        )
+    }
+
+    fn arg(mem: &Vec<i32>, p: usize, m: bool) -> i32 {
+        let val = mem[p];
+        if m {
+            val
+        } else {
+            mem[val as usize]
+        }
+    }
+
     loop {
         // if mem.len() > ip + 3 {
         //     println!(
@@ -19,62 +37,18 @@ pub fn run(mut mem: Vec<i32>, input: Vec<i32>) -> Vec<i32> {
         //         mem[ip + 3]
         //     );
         // }
+        //
+        let (m1, m2, m3, op) = parse_opcode(mem[ip]);
 
-        match mem[ip] {
+        match op {
             1 => {
-                let p1 = mem[ip + 1] as usize;
-                let p2 = mem[ip + 2] as usize;
-                let p3 = mem[ip + 3] as usize;
-                mem[p3] = mem[p1] + mem[p2];
-                ip += 4;
-            }
-            101 => {
-                let p1 = mem[ip + 1];
-                let p2 = mem[ip + 2] as usize;
-                let p3 = mem[ip + 3] as usize;
-                mem[p3] = p1 + mem[p2];
-                ip += 4;
-            }
-            1001 => {
-                let p1 = mem[ip + 1] as usize;
-                let p2 = mem[ip + 2];
-                let p3 = mem[ip + 3] as usize;
-                mem[p3] = mem[p1] + p2;
-                ip += 4;
-            }
-            1101 => {
-                let p1 = mem[ip + 1];
-                let p2 = mem[ip + 2];
-                let p3 = mem[ip + 3] as usize;
-                mem[p3] = p1 + p2;
+                let p = mem[ip + 3] as usize;
+                mem[p] = arg(&mem, ip + 1, m1) + arg(&mem, ip + 2, m2);
                 ip += 4;
             }
             2 => {
-                let p1 = mem[ip + 1] as usize;
-                let p2 = mem[ip + 2] as usize;
-                let p3 = mem[ip + 3] as usize;
-                mem[p3] = mem[p1] * mem[p2];
-                ip += 4;
-            }
-            102 => {
-                let p1 = mem[ip + 1];
-                let p2 = mem[ip + 2] as usize;
-                let p3 = mem[ip + 3] as usize;
-                mem[p3] = p1 * mem[p2];
-                ip += 4;
-            }
-            1002 => {
-                let p1 = mem[ip + 1] as usize;
-                let p2 = mem[ip + 2];
-                let p3 = mem[ip + 3] as usize;
-                mem[p3] = mem[p1] * p2;
-                ip += 4;
-            }
-            1102 => {
-                let p1 = mem[ip + 1];
-                let p2 = mem[ip + 2];
-                let p3 = mem[ip + 3] as usize;
-                mem[p3] = p1 * p2;
+                let p = mem[ip + 3] as usize;
+                mem[p] = arg(&mem, ip + 1, m1) * arg(&mem, ip + 2, m2);
                 ip += 4;
             }
             3 => {
@@ -84,143 +58,39 @@ pub fn run(mut mem: Vec<i32>, input: Vec<i32>) -> Vec<i32> {
                 inp += 1;
             }
             4 => {
-                let p1 = mem[ip + 1] as usize;
-                output.push(mem[p1]);
-                ip += 2;
-            }
-            104 => {
-                let p1 = mem[ip + 1];
-                output.push(p1);
+                output.push(arg(&mem, ip + 1, m1));
                 ip += 2;
             }
             5 => {
-                let p1 = mem[ip + 1] as usize;
-                let p2 = mem[ip + 2] as usize;
-                if mem[p1] != 0 {
-                    ip = mem[p2] as usize;
-                } else {
-                    ip += 3;
-                }
-            }
-            105 => {
-                let p1 = mem[ip + 1];
-                let p2 = mem[ip + 2] as usize;
-                if p1 != 0 {
-                    ip = mem[p2] as usize;
-                } else {
-                    ip += 3;
-                }
-            }
-            1005 => {
-                let p1 = mem[ip + 1] as usize;
-                let p2 = mem[ip + 2];
-                if mem[p1] != 0 {
-                    ip = p2 as usize;
-                } else {
-                    ip += 3;
-                }
-            }
-            1105 => {
-                let p1 = mem[ip + 1];
-                let p2 = mem[ip + 2];
-                if p1 != 0 {
-                    ip = p2 as usize;
+                if arg(&mem, ip + 1, m1) != 0 {
+                    ip = arg(&mem, ip + 2, m2) as usize;
                 } else {
                     ip += 3;
                 }
             }
             6 => {
-                let p1 = mem[ip + 1] as usize;
-                let p2 = mem[ip + 2] as usize;
-                if mem[p1] == 0 {
-                    ip = mem[p2] as usize;
+                if arg(&mem, ip + 1, m1) == 0 {
+                    ip = arg(&mem, ip + 2, m2) as usize;
                 } else {
                     ip += 3;
                 }
             }
-            106 => {
-                let p1 = mem[ip + 1];
-                let p2 = mem[ip + 2] as usize;
-                if p1 == 0 {
-                    ip = mem[p2] as usize;
-                } else {
-                    ip += 3;
-                }
-            }
-            1006 => {
-                let p1 = mem[ip + 1] as usize;
-                let p2 = mem[ip + 2];
-                if mem[p1] == 0 {
-                    ip = p2 as usize;
-                } else {
-                    ip += 3;
-                }
-            }
-            1106 => {
-                let p1 = mem[ip + 1];
-                let p2 = mem[ip + 2];
-                if p1 == 0 {
-                    ip = p2 as usize;
-                } else {
-                    ip += 3;
-                }
-            }
-            // less than
             7 => {
-                let p1 = mem[ip + 1] as usize;
-                let p2 = mem[ip + 2] as usize;
-                let p3 = mem[ip + 3] as usize;
-                mem[p3] = if mem[p1] < mem[p2] { 1 } else { 0 };
+                let p = mem[ip + 3] as usize;
+                mem[p] = if arg(&mem, ip + 1, m1) < arg(&mem, ip + 2, m2) {
+                    1
+                } else {
+                    0
+                };
                 ip += 4;
             }
-            107 => {
-                let p1 = mem[ip + 1];
-                let p2 = mem[ip + 2] as usize;
-                let p3 = mem[ip + 3] as usize;
-                mem[p3] = if p1 < mem[p2] { 1 } else { 0 };
-                ip += 4;
-            }
-            1007 => {
-                let p1 = mem[ip + 1] as usize;
-                let p2 = mem[ip + 2];
-                let p3 = mem[ip + 3] as usize;
-                mem[p3] = if mem[p1] < p2 { 1 } else { 0 };
-                ip += 4;
-            }
-            1107 => {
-                let p1 = mem[ip + 1];
-                let p2 = mem[ip + 2];
-                let p3 = mem[ip + 3] as usize;
-                mem[p3] = if p1 < p2 { 1 } else { 0 };
-                ip += 4;
-            }
-            // equal
             8 => {
-                let p1 = mem[ip + 1] as usize;
-                let p2 = mem[ip + 2] as usize;
-                let p3 = mem[ip + 3] as usize;
-                mem[p3] = if mem[p1] == mem[p2] { 1 } else { 0 };
-                ip += 4;
-            }
-            108 => {
-                let p1 = mem[ip + 1];
-                let p2 = mem[ip + 2] as usize;
-                let p3 = mem[ip + 3] as usize;
-                mem[p3] = if p1 == mem[p2] { 1 } else { 0 };
-                ip += 4;
-            }
-            1008 => {
-                let p1 = mem[ip + 1] as usize;
-                let p2 = mem[ip + 2];
-                let p3 = mem[ip + 3] as usize;
-                mem[p3] = if mem[p1] == p2 { 1 } else { 0 };
-                ip += 4;
-            }
-            1108 => {
-                let p1 = mem[ip + 1];
-                let p2 = mem[ip + 2];
-                let p3 = mem[ip + 3] as usize;
-                mem[p3] = if p1 == p2 { 1 } else { 0 };
+                let p = mem[ip + 3] as usize;
+                mem[p] = if arg(&mem, ip + 1, m1) == arg(&mem, ip + 2, m2) {
+                    1
+                } else {
+                    0
+                };
                 ip += 4;
             }
             99 => break,
