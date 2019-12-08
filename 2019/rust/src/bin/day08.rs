2324d467d9fc24fb211ec99257ea95202425370b
Łukasz Adamczak <lukasz@czak.pl>
2019-12-08 08:54:26 +0100

    2019 day 08 part 2

--- 2019/rust/src/bin/day08.rs
+++ 2019/rust/src/bin/day08.rs
@@ -5,6 +5,16 @@ fn count(layer: &[u32], pixel: u32) -> usize {
     layer.iter().filter(|&&p| p == pixel).count()
 }
 
+fn color(image: &[u32], pos: usize) -> u32 {
+    *image
+        .iter()
+        .skip(pos)
+        .step_by(WIDTH * HEIGHT)
+        .filter(|&&p| p != 2)
+        .next()
+        .unwrap()
+}
+
 fn main() {
     let image: Vec<u32> = include_str!("../../../in/day08.in")
         .trim()
@@ -18,4 +28,15 @@ fn main() {
         .unwrap();
 
     println!("Part 1: {}", count(best_layer, 1) * count(best_layer, 2));
+
+    let pixels: Vec<char> = (0..(WIDTH * HEIGHT))
+        .map(|pos| color(&image, pos))
+        .map(|n| if n == 0 { ' ' } else { '#' })
+        .collect();
+
+    println!("Part 2:");
+    pixels
+        .chunks(WIDTH)
+        .map(|c| c.iter().collect::<String>())
+        .for_each(|c| println!("{:?}", c));
 }
