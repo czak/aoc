440832eec68841362586fa364f56dfb6a7a5e7e0
Łukasz Adamczak <lukasz@czak.pl>
2019-12-10 18:59:53 +0100

    2019 day 10 part 2

--- 2019/rust/src/bin/day10.rs
+++ 2019/rust/src/bin/day10.rs
@@ -1,4 +1,5 @@
-use std::collections::HashSet;
+use std::collections::{BinaryHeap, HashMap, HashSet};
+use std::f32::consts::PI;
 
 #[allow(dead_code)]
 const EX1: &str = ".#..#
@@ -41,52 +42,118 @@ const EX3: &str = ".#..##.###...#######
 #.#.#.#####.####.###
 ###.##.####.##.#..##";
 
-type Point = (f64, f64);
+#[allow(dead_code)]
+const EX4: &str = ".#....#####...#..
+##...##.#####..##
+##...#...#.#####.
+..#.....#...###..
+..#.#.....#....##";
 
-fn load(s: &str) -> Vec<Point> {
-    let mut res = vec![];
-    for (y, line) in s.lines().enumerate() {
-        for (x, ch) in line.chars().enumerate() {
-            if ch == '#' {
-                res.push((x as f64, y as f64));
-            }
-        }
-    }
-    res
+macro_rules! fixed {
+    ($f:expr) => {
+        ($f * 1_000_000.0) as i32
+    };
 }
 
-fn direction(origin: Point, target: Point) -> (i64, i64) {
-    if origin == target {
-        return (0, 0);
-    }
-    let dir = (target.0 - origin.0, target.1 - origin.1);
-    let len = dir.0.hypot(dir.1);
+type Point = (i32, i32);
+type Polar = (i32, i32);
+
+fn polar(p: Point) -> Polar {
     (
-        (dir.0 / len * 10_000_000.0).round() as i64,
-        (dir.1 / len * 10_000_000.0).round() as i64,
+        match (p.0 as f32).atan2(-p.1 as f32) {
+            angle if angle >= 0.0 => fixed!(angle),
+            angle => fixed!(2.0 * PI + (angle as f32)),
+        },
+        fixed!((p.0 as f32).hypot(p.1 as f32)),
     )
 }
 
-fn directions(origin: Point, targets: &Vec<Point>) -> HashSet<(i64, i64)> {
+fn load(s: &str) -> Vec<Point> {
+    s.lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars().enumerate().filter_map(move |(x, ch)| {
+                if ch == '#' {
+                    Some((x as i32, y as i32))
+                } else {
+                    None
+                }
+            })
+        })
+        .collect()
+}
+
+fn angles(origin: Point, targets: &[Point]) -> HashSet<i32> {
     targets
         .iter()
-        .map(|&t| direction(origin, t))
-        .filter(|&d| d != (0, 0))
+        .filter(|&&p| p != origin)
+        .map(|&p| polar((p.0 - origin.0, p.1 - origin.1)).0)
         .collect()
+}
+
+fn part1(asteroids: &[Point]) -> (Point, usize) {
+    asteroids
+        .iter()
+        .map(|&a| (a, angles(a, asteroids).len()))
+        .max_by_key(|p| p.1)
+        .unwrap()
+}
+
+fn part2(base: Point, asteroids: &[Point]) -> Vec<Point> {
+    let mut m: HashMap<i32, BinaryHeap<(i32, Point)>> = HashMap::new();
+
+    for &point in asteroids.iter().filter(|&&p| p != base) {
+        let (angle, distance) = polar((point.0 - base.0, point.1 - base.1));
+        let v = m.entry(angle).or_insert(BinaryHeap::new());
+        v.push((-distance, point));
+    }
+
+    // sort by angle
+    let mut angles: Vec<_> = m.keys().cloned().collect();
+    angles.sort();
+
+    let mut out: Vec<Point> = vec![];
+
+    while m.values().any(|v| v.len() != 0) {
+        for angle in &angles {
+            if let Some((_distance, point)) = m.get_mut(&angle).unwrap().pop() {
+                // println!("{}: {:?}", i, item);
+                out.push(point);
+                continue;
+            }
+        }
+    }
+
+    out
 }
 
 fn main() {
     let asteroids = load(include_str!("../../../in/day10.in"));
 
-    let best = asteroids
-        .iter()
-        .cloned()
-        .max_by_key(|&a| directions(a, &asteroids).len())
-        .unwrap();
+    // part 1
+
+    assert_eq!(((3, 4), 8), part1(&load(EX1)));
+    assert_eq!(((5, 8), 33), part1(&load(EX2)));
+    assert_eq!(((11, 13), 210), part1(&load(EX3)));
+
+    let (best, count) = part1(&asteroids);
+    assert_eq!((19, 11), best);
+    assert_eq!(230, count);
 
     println!(
         "Part 1: best is {:?} with {} asteroids detected",
-        best,
-        directions(best, &asteroids).len()
+        best, count
     );
+
+    // part 2
+
+    let _ex = part2((11, 13), &load(EX3));
+    assert_eq!((11, 12), _ex[0]);
+    assert_eq!((16, 9), _ex[49]);
+    assert_eq!((8, 2), _ex[199]);
+
+    let targets = part2((19, 11), &asteroids);
+    assert_eq!((12, 5), targets[199]);
+
+    println!("Part 2: {:?}", targets[199]);
 }
