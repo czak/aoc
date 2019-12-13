f9abb152b83eb10dd649757ebe5d367e71682654
Łukasz Adamczak <lukasz@czak.pl>
2019-12-13 12:23:58 +0100

    2019 day 13 part 1

---
+++ 2019/rust/src/bin/day13.rs
@@ -0,0 +1,28 @@
+use aoc2019::intcode_redux as intcode;
+use std::collections::HashSet;
+
+fn main() {
+    let mut ic = intcode::Intcode {
+        mem: intcode::parse(include_str!("../../../in/day13.in")),
+        ..Default::default()
+    };
+
+    ic.mem.resize(10000, 0);
+
+    let output: Vec<i64> = ic.collect();
+    let mut blocks: HashSet<(i64, i64)> = HashSet::new();
+
+    for chunk in output.chunks(3) {
+        let x = chunk[0];
+        let y = chunk[1];
+        let id = chunk[2];
+
+        if id == 2 {
+            blocks.insert((x, y));
+        } else {
+            blocks.remove(&(x, y));
+        }
+    }
+
+    println!("Part 1: {}", blocks.len());
+}
