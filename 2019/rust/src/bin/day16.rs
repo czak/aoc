8da61d3ca3d4af6e636291a7cd4706081c5fcfdd
Łukasz Adamczak <lukasz@czak.pl>
2019-12-16 07:15:50 +0100

    Some cleanup in Pattern

--- 2019/rust/src/bin/day16.rs
+++ 2019/rust/src/bin/day16.rs
@@ -5,11 +5,17 @@ struct Pattern {
     i: usize,
 }
 
+impl Pattern {
+    fn for_level(level: usize) -> Pattern {
+        Pattern { level, i: 1 }
+    }
+}
+
 impl Iterator for Pattern {
     type Item = i32;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let offset = (self.i / (self.level + 1)) % 4;
+        let offset = (self.i / (self.level + 1)) % BASE_PATTERN.len();
         self.i += 1;
         Some(BASE_PATTERN[offset])
     }
@@ -17,11 +23,11 @@ impl Iterator for Pattern {
 
 fn main() {
     assert_eq!(
-        vec![0, 1, 0, -1],
-        Pattern { level: 0, i: 0 }.take(4).collect::<Vec<_>>()
+        vec![1, 0, -1, 0, 1],
+        Pattern::for_level(0).take(5).collect::<Vec<_>>()
     );
     assert_eq!(
-        vec![0, 0, 1, 1, 0, 0, -1, -1, 0, 0],
-        Pattern { level: 1, i: 0 }.take(10).collect::<Vec<_>>()
+        vec![0, 1, 1, 0, 0, -1, -1, 0, 0, 1],
+        Pattern::for_level(1).take(10).collect::<Vec<_>>()
     );
 }
