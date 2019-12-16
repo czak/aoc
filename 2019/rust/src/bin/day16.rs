c4ea271fc75e0067fd35e6849195256a14364fef
Łukasz Adamczak <lukasz@czak.pl>
2019-12-16 18:33:41 +0100

    2019 day 16

--- 2019/rust/src/bin/day16.rs
+++ 2019/rust/src/bin/day16.rs
@@ -66,6 +66,21 @@ fn main() {
         "Part 1: {:?}",
         part1(parse(include_str!("../../../in/day16.in")))
     );
+
+    // part 2 examples
+    assert_eq!(
+        parse("84462026"),
+        part2(parse("03036732577212944063491565474664"))
+    );
+    assert_eq!(
+        parse("78725270"),
+        part2(parse("02935109699940807407585447034323"))
+    );
+
+    println!(
+        "Part 2: {:?}",
+        part2(parse(include_str!("../../../in/day16.in")))
+    );
 }
 
 fn parse(s: &str) -> Vec<i32> {
@@ -84,4 +99,41 @@ fn part1(input: Vec<i32>) -> Vec<i32> {
         .copied()
         .take(8)
         .collect()
+}
+
+//  0: [ ... ,     0,     0,     0,     1] => [ ... ,     0,     0,     0,     1]
+//  1: [ ... ,     1,     1,     1,     1] => [ ... ,     1,     1,     1,     1]
+//  2: [ ... ,     4,     3,     2,     1] => [ ... ,     4,     3,     2,     1]
+//  3: [ ... ,    10,     6,     3,     1] => [ ... ,     0,     6,     3,     1]
+//  4: [ ... ,    20,    10,     4,     1] => [ ... ,     0,     0,     4,     1]
+//  5: [ ... ,    35,    15,     5,     1] => [ ... ,     5,     5,     5,     1]
+//  6: [ ... ,    56,    21,     6,     1] => [ ... ,     6,     1,     6,     1]
+//  7: [ ... ,    84,    28,     7,     1] => [ ... ,     4,     8,     7,     1]
+//  8: [ ... ,   120,    36,     8,     1] => [ ... ,     0,     6,     8,     1]
+//  9: [ ... ,   165,    45,     9,     1] => [ ... ,     5,     5,     9,     1]
+// 10: [ ... ,   220,    55,    10,     1] => [ ... ,     0,     5,    10,     1]
+fn part2(input: Vec<i32>) -> Vec<i32> {
+    let mut signal: Vec<i32> = input
+        .iter()
+        .copied()
+        .cycle()
+        .take(10000 * input.len())
+        .collect::<Vec<_>>();
+
+    for _n in 0..100 {
+        // println!("{}", _n);
+
+        for i in (0..signal.len() - 1).rev() {
+            signal[i] = (signal[i + 1] + signal[i]) % 10;
+        }
+    }
+
+    let offset = input[0..7].iter().fold(0, |acc, &n| 10 * acc + n as usize);
+
+    signal
+        .iter()
+        .copied()
+        .skip(offset)
+        .take(8)
+        .collect::<Vec<_>>()
 }
