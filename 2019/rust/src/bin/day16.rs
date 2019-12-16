94edd0bcdf735cf6765c3cf2b9ec1335b28607a6
Łukasz Adamczak <lukasz@czak.pl>
2019-12-16 07:43:10 +0100

    2019 day 16 part 1

--- 2019/rust/src/bin/day16.rs
+++ 2019/rust/src/bin/day16.rs
@@ -21,6 +21,22 @@ impl Iterator for Pattern {
     }
 }
 
+fn fft(signal: &[i32]) -> Vec<i32> {
+    let mut output: Vec<i32> = vec![];
+    for i in 0..signal.len() {
+        let pat = Pattern::for_level(i);
+        output.push(
+            signal
+                .iter()
+                .zip(pat)
+                .fold(0, |acc, (a, b)| acc + a * b)
+                .abs()
+                % 10,
+        );
+    }
+    output
+}
+
 fn main() {
     assert_eq!(
         vec![1, 0, -1, 0, 1],
@@ -30,4 +46,42 @@ fn main() {
         vec![0, 1, 1, 0, 0, -1, -1, 0, 0, 1],
         Pattern::for_level(1).take(10).collect::<Vec<_>>()
     );
+
+    // parsing
+    assert_eq!(vec![1, 2, 3, 4, 5, 6, 7, 8], parse("12345678"));
+
+    // fft
+    assert_eq!(
+        vec![3, 4, 0, 4, 0, 4, 3, 8],
+        fft(&fft(&vec![1, 2, 3, 4, 5, 6, 7, 8]))
+    );
+
+    // part 1 examples
+    assert_eq!(
+        parse("24176176"),
+        part1(parse("80871224585914546619083218645595"))
+    );
+
+    println!(
+        "Part 1: {:?}",
+        part1(parse(include_str!("../../../in/day16.in")))
+    );
+}
+
+fn parse(s: &str) -> Vec<i32> {
+    s.trim()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as i32)
+        .collect()
+}
+
+fn part1(input: Vec<i32>) -> Vec<i32> {
+    std::iter::successors(Some(input), |i| Some(fft(i)))
+        .skip(100)
+        .next()
+        .unwrap()
+        .iter()
+        .copied()
+        .take(8)
+        .collect()
 }
