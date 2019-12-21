bc55948ae767750946e0dfb82a0bb8b8598da1d4
Łukasz Adamczak <lukasz@czak.pl>
2019-12-21 09:20:08 +0100

    2019 day 21 part 2

--- 2019/rust/src/bin/day21.rs
+++ 2019/rust/src/bin/day21.rs
@@ -1,8 +1,7 @@
-#![allow(unused_imports)]
 use aoc2019::intcode_redux as intcode;
-use std::io::{self, Read};
 
-const PROGRAM: &str = "NOT A J
+// J := (!1 | !2 | !3) & 4
+const PROGRAM1: &str = "NOT A J
 NOT B T
 OR T J
 NOT C T
@@ -11,16 +10,37 @@ AND D J
 WALK
 ";
 
-fn main() {
-    // let mut buffer = String::new();
-    // io::stdin().read_to_string(&mut buffer).unwrap();
+// J := (!1 | !2 | !3) & 4 & ((!5 & 8) | 5)
+const PROGRAM2: &str = "NOT A J
+NOT B T
+OR T J
+NOT C T
+OR T J
+AND D J
+NOT E T
+AND H T
+OR E T
+AND T J
+RUN
+";
 
+fn main() {
     let mut ic = intcode::Intcode {
         mem: intcode::parse(include_str!("../../../in/day21.in")),
-        input: PROGRAM.chars().map(|c| c as i64).collect(),
+        input: PROGRAM1.chars().map(|c| c as i64).collect(),
         ..Default::default()
     };
     ic.mem.resize(5000, 0);
 
     println!("Part 1: {}", ic.last().unwrap());
+
+    // Part 2
+    let mut ic = intcode::Intcode {
+        mem: intcode::parse(include_str!("../../../in/day21.in")),
+        input: PROGRAM2.chars().map(|c| c as i64).collect(),
+        ..Default::default()
+    };
+    ic.mem.resize(5000, 0);
+
+    println!("Part 2: {}", ic.last().unwrap());
 }
