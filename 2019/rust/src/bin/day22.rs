3122eddff15074a9f7aa3cae5e530a9ec614e751
Łukasz Adamczak <lukasz@czak.pl>
2019-12-22 18:17:31 +0100

    Remove the vecs

--- 2019/rust/src/bin/day22.rs
+++ 2019/rust/src/bin/day22.rs
@@ -27,52 +27,9 @@ impl FromStr for Shuffle {
 fn main() {
     // Part 1
 
-    let deck: Vec<i32> = (0..).take(10007).collect();
     let seq = parse(include_str!("../../../in/day22.in"));
-    let res = shuffle(deck, &seq);
-    println!("Part 1: {:?}", res.iter().position(|&n| n == 2019).unwrap());
-
     println!("Part 1: {}", reindex_seq(2019, 10007, &seq));
     assert_eq!(2519, reindex_seq(2019, 10007, &seq));
-
-    // // Part 2
-    //
-    // let mut deck: Vec<i32> = (0..).take(10).collect();
-    // let seq = parse(include_str!("../../../in/day22.in"));
-    // deck = shuffle(deck, &seq);
-}
-
-fn shuffle(mut deck: Vec<i32>, seq: &Vec<Shuffle>) -> Vec<i32> {
-    use Shuffle::*;
-    for shuffle in seq {
-        deck = match shuffle {
-            &DealIntoNewStack => deck.into_iter().rev().collect(),
-            &Cut(n) => {
-                let split = if n >= 0 { n } else { deck.len() as i32 + n };
-                deck.iter()
-                    .skip(split as usize)
-                    .chain(deck.iter().take(split as usize))
-                    .copied()
-                    .collect()
-            }
-            &DealWithIncrement(n) => {
-                let indexes = (0..)
-                    .take(deck.len())
-                    .cycle()
-                    .step_by(n as usize)
-                    .take(deck.len());
-                let mut deck: Vec<i32> = deck.into_iter().rev().collect();
-                let mut res = vec![];
-                res.resize(deck.len(), 0);
-                for i in indexes {
-                    res[i] = deck.pop().unwrap();
-                }
-                res
-            }
-        };
-        // println!("{:?}: {:?}", shuffle, deck);
-    }
-    deck
 }
 
 fn parse(s: &str) -> Vec<Shuffle> {
@@ -107,58 +64,6 @@ cut 3
 deal with increment 9
 deal with increment 3
 cut -1";
-
-#[test]
-fn test_shuffling() {
-    let deck: Vec<i32> = (0..).take(10).collect();
-    assert_eq!(
-        vec![9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
-        shuffle(deck, &vec![Shuffle::DealIntoNewStack])
-    );
-
-    let deck: Vec<i32> = (0..).take(10).collect();
-    assert_eq!(
-        vec![3, 4, 5, 6, 7, 8, 9, 0, 1, 2],
-        shuffle(deck, &vec![Shuffle::Cut(3)])
-    );
-
-    let deck: Vec<i32> = (0..).take(10).collect();
-    assert_eq!(
-        vec![6, 7, 8, 9, 0, 1, 2, 3, 4, 5],
-        shuffle(deck, &vec![Shuffle::Cut(-4)])
-    );
-
-    let deck: Vec<i32> = (0..).take(10).collect();
-    assert_eq!(
-        vec![0, 7, 4, 1, 8, 5, 2, 9, 6, 3],
-        shuffle(deck, &vec![Shuffle::DealWithIncrement(3)])
-    );
-
-    let deck = vec![1, 0, 9, 8, 7, 6, 5, 4, 3, 2];
-    assert_eq!(
-        vec![1, 8, 5, 2, 9, 6, 3, 0, 7, 4],
-        shuffle(deck, &vec![Shuffle::DealWithIncrement(7)])
-    );
-
-    let deck = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    assert_eq!(
-        //   0  1  2  3  4  5  6  7  8  9
-        vec![0, 9, 8, 7, 6, 5, 4, 3, 2, 1],
-        shuffle(deck, &vec![Shuffle::DealWithIncrement(9)])
-    );
-
-    let seq = parse(EX1);
-    let deck: Vec<i32> = (0..).take(10).collect();
-    assert_eq!(vec![0, 3, 6, 9, 2, 5, 8, 1, 4, 7], shuffle(deck, &seq));
-
-    let seq = parse(EX2);
-    let deck: Vec<i32> = (0..).take(10).collect();
-    assert_eq!(vec![3, 0, 7, 4, 1, 8, 5, 2, 9, 6], shuffle(deck, &seq));
-
-    let seq = parse(EX3);
-    let deck: Vec<i32> = (0..).take(10).collect();
-    assert_eq!(vec![6, 3, 0, 7, 4, 1, 8, 5, 2, 9], shuffle(deck, &seq));
-}
 
 fn reindex(i: i32, size: i32, shuffle: Shuffle) -> i32 {
     use Shuffle::*;
