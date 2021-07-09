8d23563bea10fe3b0cb706b4c66ac68f775449a2
Łukasz Adamczak <14021+czak@users.noreply.github.com>
2021-07-09 13:24:09 +0200

    2016 day 1 part 2 in Zig

--- 2016/zig/day01.zig
+++ 2016/zig/day01.zig
@@ -4,6 +4,7 @@ const input = "R4, R1, L2, R1, L1, L1, R1, L5, R1, R5, L2, R3, L3, L4, R4, R4, R
 
 pub fn main() void {
     std.debug.print("Part 1: {}\n", .{distance(input)});
+    std.debug.print("Part 2: {}\n", .{traverse(input)});
 }
 
 const Direction = enum(u2) {
@@ -43,14 +44,62 @@ fn distance(steps: []const u8) i32 {
     return (abs(x) catch unreachable) + (abs(y) catch unreachable);
 }
 
-test "example 1" {
+const Point = struct {
+    x: i32,
+    y: i32,
+
+    fn distance(self: Point) i32 {
+        const abs = std.math.absInt;
+        return (abs(self.x) catch unreachable) + (abs(self.y) catch unreachable);
+    }
+};
+
+fn traverse(steps: []const u8) i32 {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    var pos = Point{ .x = 0, .y = 0 };
+    var dir: Direction = .north;
+    var visited = std.AutoHashMap(Point, void).init(&arena.allocator);
+
+    var it = std.mem.split(steps, ", ");
+    while (it.next()) |step| {
+        dir = if (step[0] == 'R') dir.right() else dir.left();
+        var dist = std.fmt.parseInt(i32, step[1..], 10) catch unreachable;
+
+        while (dist > 0) {
+            switch (dir) {
+                .north => pos.y += 1,
+                .east => pos.x += 1,
+                .south => pos.y -= 1,
+                .west => pos.x -= 1,
+            }
+
+            if (visited.contains(pos)) {
+                return pos.distance();
+            }
+
+            visited.put(pos, {}) catch unreachable;
+
+            dist -= 1;
+        }
+    }
+
+    unreachable;
+}
+
+test "part 1, example 1" {
     try std.testing.expect(distance("R2, L3") == 5);
 }
 
-test "example 2" {
+test "part 1, example 2" {
     try std.testing.expect(distance("R2, R2, R2") == 2);
 }
 
-test "example 3" {
+test "part 1, example 3" {
     try std.testing.expect(distance("R5, L5, R5, R3") == 12);
+}
+
+test "part 2, example 1" {
+    try std.testing.expect(traverse("R8, R4, R4, R8") == 4);
 }
