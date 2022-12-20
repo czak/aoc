d115ff95ddfa7bb287311e1c3fab7d769b60e6d8
Łukasz Adamczak <14021+czak@users.noreply.github.com>
2022-12-20 21:27:10 +0100

    2022 day 12

--- 2022/cpp/day12.cpp
+++ 2022/cpp/day12.cpp
@@ -4,77 +4,95 @@ istringstream example{R"(Sabqponm
 abcryxxl
 accszExk
 acctuvwj
-abdefghi)"};
+abdefghi
+)"};
 
-using Grid = vector<vector<int>>;
 using Point = pair<int, int>;
-using Graph = unordered_map<int, unordered_set<int>>; // 100*y + x
+using Grid = vector<vector<int>>;
+using Graph = vector<pair<int, int>>;
 
-tuple<Grid, Point, Point> parse(istream& input)
+tuple<Grid, int, int> parse(istream& input)
 {
-  int x = 0, y = 0;
   Grid grid{{}};
-  Point start{}, end{};
+  Point start, end;
+  int x = 0, y = 0, width = 0;
 
   char c;
-  while (example.get(c)) {
+  while (input.get(c)) {
     if (c == '\n') {
+      input.peek();
+      if (input.eof()) break;
       grid.push_back({});
+      width = x;
       x = 0;
       y++;
     } else {
-      auto& row = grid.back();
       if (c == 'S') {
         start = {x, y};
-        row.push_back(0);
+        grid.back().push_back(0);
       } else if (c == 'E') {
         end = {x, y};
-        row.push_back(25);
+        grid.back().push_back(25);
       } else {
-        row.push_back(c - 'a');
+        grid.back().push_back(c - 'a');
       }
       x++;
     }
   }
 
-  return {grid, start, end};
+  return {grid, width * start.second + start.first, width * end.second + end.first};
 }
 
-Graph make_graph(const Grid& grid)
+tuple<Graph, int, int> make_graph(const Grid& grid)
 {
   Graph g{};
-  for (int y = 0; y < grid.size(); y++) {
-    for (int x = 0; x < grid[0].size(); x++) {
-      unordered_set<int> vs{};
-      int loc = 100 * y + x;
+  int width = grid[0].size();
+  int height = grid.size();
+
+  for (int y = 0; y < height; y++) {
+    for (int x = 0; x < width; x++) {
+      int l = width * y + x;
       int elev = grid[y][x];
 
-      // up
-      if (y > 0 && grid[y - 1][x] - elev <= 1) vs.insert(loc - 100);
-
-      // down
-      if (y < grid.size() - 1 && grid[y + 1][x] - elev <= 1) vs.insert(loc + 100);
-
-      // left
-      if (x > 0 && grid[y][x - 1] - elev <= 1) vs.insert(loc - 1);
-
-      // right
-      if (x < grid[0].size() - 1 && grid[y][x + 1] - elev <= 1) vs.insert(loc + 1);
-
-      g[loc] = vs;
+      // clang-format off
+      if (y > 0          && elev - grid[y - 1][x] <= 1) g.push_back({l, l - width});
+      if (y < height - 1 && elev - grid[y + 1][x] <= 1) g.push_back({l, l + width});
+      if (x > 0          && elev - grid[y][x - 1] <= 1) g.push_back({l, l - 1});
+      if (x < width - 1  && elev - grid[y][x + 1] <= 1) g.push_back({l, l + 1});
+      // clang-format on
     }
   }
-  return g;
+
+  return {g, width, height};
+}
+
+vector<int> solve(const Graph& graph, int n, int start)
+{
+  vector<int> distance(n, 1000000);
+  distance[start] = 0;
+
+  for (int i = 0; i < n; i++) {
+    for (auto [a, b] : graph) {
+      distance[b] = min(distance[b], distance[a] + 1);
+    }
+  }
+
+  return distance;
 }
 
 int main()
 {
-  auto [grid, start, end] = parse(example);
-  auto graph = make_graph(grid);
+  auto [grid, start, end] = parse(cin);
+  auto [graph, width, height] = make_graph(grid);
+  auto distance = solve(graph, width * height, end);
 
-  dbg(grid);
-  dbg(start);
-  dbg(end);
+  cout << "Part 1: " << distance[start] << '\n';
 
-  dbg(graph[304]);
+  int best = 100000;
+  for (int i = 0; i < width * height; i++) {
+    if (grid[i / width][i % width] != 0) continue;
+    best = min(best, distance[i]);
+  }
+
+  cout << "Part 2: " << best << '\n';
 }
